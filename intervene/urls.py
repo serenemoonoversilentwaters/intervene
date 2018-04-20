75830d6ee6032f4501fc62e7ba6b3b42c56154fe
Khamin Nathabheem <khamin.nathabheem@icsd.k12.ny.us>
2018-04-20 13:50:04 -0400

    V.0.4
    Life is miserable. CSS is killing me. Send help.

--- intervene/urls.py
+++ intervene/urls.py
@@ -19,12 +19,11 @@ from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import RedirectView
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-    path('', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
